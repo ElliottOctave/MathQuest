38f46e113438af66dd2b7073d5357c8c35456e83
marouane3105 <marouane.benslimane@outlook.com>
2025-05-17 15:51:25 +0200

    fix: wrong image name fixed

--- auth.js
+++ auth.js
@@ -60,7 +60,7 @@ export async function handleRegister(event) {
       difficulty_game12: 1,
       unlocked_features:[0],
       profile_pictures: [0,0,0,0,0,0,0,0,0,0,0,0],
-      current_profile_picture: "default_profile_picture.png"
+      current_profile_picture: "default_profile_pic.png"
     });
 
     alert('Registration successful! You can now log in.');
